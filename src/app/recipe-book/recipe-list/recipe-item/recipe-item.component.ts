608eefb3e704daa398f79417e34b4875d08b0cdb
Fady Soliman <fadysoliman160@hotmail.com>
2018-05-06 03:15:24 +0200

    removing redundant code

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, Input } from '@angular/core';
 import {Recipe} from "../../recipe.model";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,13 +10,9 @@ export class RecipeItemComponent implements OnInit {
 
   @Input() recipe : Recipe;
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onClickRecipe(){
-    this.router.navigate(['/recipes', this.recipe.id]);
   }
 
 }
